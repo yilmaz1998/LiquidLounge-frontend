1cb311f9483f822847034362f2e5e62aadccc064
yilmaz1998 <snn.ylmz9804@gmail.com>
2024-05-29 22:51:54 -0400

    Classic drinks fetching

--- liquid-lounge/src/pages/classics/ClassicsPage.jsx
+++ liquid-lounge/src/pages/classics/ClassicsPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const ClassicsPage = () => {
-  return (
-    <div>ClassicsPage</div>
-  )
-}
+  const [classicDrinks, setClassicDrinks] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-export default ClassicsPage
+  useEffect(() => {
+    fetch('http://localhost:3000/classic', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Failed to fetch')
+        }
+      })
+      .then((data) => setClassicDrinks(data))
+      .catch((error) => console.error('Error fetching data:', error))
+      .finally(() => setIsLoading(false))
+  }, []);
+
+  return (
+    <div>
+      <h1>Classic Cocktails</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {classicDrinks.map((drink, index) => (
+            <li key={index}>
+              <Link to={`/classics/${drink._id}`}>
+                <h2>{drink.name}</h2>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ClassicsPage;
+
--- liquid-lounge/src/pages/classics/ShowClassics.jsx
+++ liquid-lounge/src/pages/classics/ShowClassics.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const ShowClassics = () => {
-  return (
-    <div>ShowClassics</div>
-  )
-}
+  const { id } = useParams()
+  const [classicDrink, setClassicDrink] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
-export default ShowClassics
+  useEffect(() => {
+    fetch(`http://localhost:3000/classic/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Failed to fetch');
+        }
+      })
+      .then((data) => setClassicDrink(data))
+      .catch((error) => console.error('Error fetching data:', error))
+      .finally(() => setIsLoading(false))
+  }, [id])
+
+  return (
+    <div>
+      <h1>Classic Cocktail Details</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : classicDrink ? (
+        <div>
+          <h2>{classicDrink.name}</h2>
+          <img src={classicDrink.img}></img>
+          <p>{classicDrink.ingredients}</p>
+          <p>{classicDrink.method}</p>
+        </div>
+      ) : (
+        <p>No data found for this classic cocktail.</p>
+      )}
+    </div>
+  );
+};
+
+export default ShowClassics;
